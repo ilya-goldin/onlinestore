cb4efd2a0daa501da36f50d32871204dc1236952
ilya-goldin <rtrwv88@gmail.com>
2021-10-06 10:25:10 +0300

    fix: add basenames to the ViewSets

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from rest_framework.routers import DefaultRouter
 from .api_views import ProductViewSet, ReviewViewSet, OrderViewSet, CollectionViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('product-reviews', ReviewViewSet)
-router.register('orders', OrderViewSet)
-router.register('product-collections', CollectionViewSet)
+router.register('products', ProductViewSet, basename='products')
+router.register('product-reviews', ReviewViewSet, basename='product-reviews')
+router.register('orders', OrderViewSet, basename='orders')
+router.register('product-collections', CollectionViewSet, basename='product-collections')
 
 urlpatterns = router.urls
