96d60ba4e4b9b1bf8fea7b11606d56edcdacfdb6
ilya-goldin <rtrwv88@gmail.com>
2021-10-06 13:48:37 +0300

    feat: add Product filterset class in Product view set

--- products/api_views.py
+++ products/api_views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
+from .filters import ProductFilter
 from .models import Product, Review, Order, Collection
 from .premissions import IsCreator
 from .serializers import ProductSerializer, ReviewSerializer, OrderSerializer, CollectionSerializer
@@ -10,6 +11,7 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_permissions(self):
         if self.action in ["create", "update", "partial_update", 'destroy']:
