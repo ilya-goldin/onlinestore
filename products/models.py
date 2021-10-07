06ac226b1c9340c29ba543f4e33a669219e8a322
ilya-goldin <rtrwv88@gmail.com>
2021-10-07 22:06:49 +0300

    fix: change product field settings in OrderProduct model

--- products/models.py
+++ products/models.py
@@ -133,7 +133,7 @@ class OrderProducts(models.Model):
     )
     product = models.ForeignKey(
         Product,
-        on_delete=models.CASCADE,
+        on_delete=models.RESTRICT,
     )
     qty = models.PositiveIntegerField(
         default=1,
