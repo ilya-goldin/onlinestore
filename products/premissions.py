8cdf75cdfadb66ce3091d935feb4e138bc37a52b
ilya-goldin <rtrwv88@gmail.com>
2021-10-07 17:56:47 +0300

    feat: add IsOrderCreator permissions check

--- products/premissions.py
+++ products/premissions.py
@@ -5,4 +5,9 @@ class IsCreator(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.user_id == request.user.id
+        return obj.user_id == request.user.id or request.user.is_staff
+
+
+class IsOrderCreator(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user_id == request.user.id or request.user.is_staff
