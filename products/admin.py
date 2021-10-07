2f77652f0396af54ef29409f0ab09bba7b8c02ee
ilya-goldin <rtrwv88@gmail.com>
2021-10-07 22:15:44 +0300

    fix: remove calculate order value logic by post_save and pre_delete signals

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.db.models.signals import post_save, pre_delete
 from .models import Product, Collection, Review, Order, OrderProducts
 
 
@@ -27,22 +26,3 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [ProductInLine]
     list_display = ('user', 'status', 'order_value', 'created_at', 'updated_at')
 
-
-def calculate_total_add(sender, instance, **kwargs):
-    price = instance.product.price
-    qty = instance.qty
-    order = instance.order
-    order.order_value += (price * qty)
-    order.save()
-
-
-def calculate_total_remove(sender, instance, **kwargs):
-    price = instance.product.price
-    qty = instance.qty
-    order = instance.order
-    order.order_value -= (price * qty)
-    order.save()
-
-
-post_save.connect(calculate_total_add, sender=OrderProducts)
-pre_delete.connect(calculate_total_remove, sender=OrderProducts)
