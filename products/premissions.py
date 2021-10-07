58690d93dc400a17f1a57c7fe94aae03739c2d2f
ilya-goldin <rtrwv88@gmail.com>
2021-10-07 13:52:04 +0300

    fix: change user_id param

--- products/premissions.py
+++ products/premissions.py
@@ -2,9 +2,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsCreator(BasePermission):
-    """Определение доступа к объявлению"""
-
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.creator_id == request.user.id
+        return obj.user_id == request.user.id
