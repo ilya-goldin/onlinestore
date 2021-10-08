ee575a7e3a32b9102cfa4ebdfa22ef7839b8939a
ilya-goldin <rtrwv88@gmail.com>
2021-10-08 12:28:03 +0300

    feat: register token viewset url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .api_views import ProductViewSet, ReviewViewSet, OrderViewSet, CollectionViewSet
+from .api_views import ProductViewSet, ReviewViewSet,\
+    OrderViewSet, CollectionViewSet, TokenViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
 router.register('product-reviews', ReviewViewSet, basename='product-reviews')
 router.register('orders', OrderViewSet, basename='orders')
 router.register('product-collections', CollectionViewSet, basename='product-collections')
+router.register('token', TokenViewSet, basename='token')
 
 urlpatterns = router.urls
